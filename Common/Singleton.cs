4c5cdca58ce463012402b347ce4c4759d802915b
LINKLIU <mucylulu@163.com>
2022-04-23 19:40:40 +0800

    添加一个单例模板类

---
+++ Common/Singleton.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+	public class Singleton<T> where T : class, new()
+	{
+		private static readonly object Sync = new object();
+		private static T instance = null;
+		public static T Instance
+		{
+			get 
+			{
+				if (instance == null)
+				{
+					lock (Sync)
+					{
+						if (instance == null)
+						{
+							instance = new T();
+						}
+					}
+				}
+				return instance;
+			}
+		}
+	}
+}
